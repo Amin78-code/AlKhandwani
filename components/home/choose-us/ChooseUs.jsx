b035956f3a95233d11dfc0c6989effe0b827d3e2
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-10-21 14:18:42 +0500

    section added

--- components/home/choose-us/ChooseUs.jsx
+++ components/home/choose-us/ChooseUs.jsx
@@ -34,35 +34,20 @@ const ChooseUs = () => {
 
           <div className="relative z-[3] pt-[50px] ml-[96px]">
             <Slider {...settings}>
-<<<<<<< Updated upstream
             <div>
                 <div className="w-[360px] h-[379px] flex justify-center flex-col gap-y-[20px] items-center  shadow-[0px_0px_10px_0px_rgb(255,99,99,35%)] hover:bg-[#d4a051] hover:text-white  bg-white p-10 group  ">
                   <i className="fa fa-car text-[35px] text-[#d4a051] border-[2px] border-[#d4a051] group-hover:text-white group-hover:border-[#FFF] rounded-full p-5"></i>
-=======
-              <div>
-                <div className="w-[360px] h-[379px] flex justify-center flex-col gap-y-[20px] items-center  shadow-[0px_0px_10px_0px_rgb(255,99,99,35%)] hover:bg-[#d4a051]  bg-white p-10  ">
-                  <i className="fa fa-car text-[35px] text-[#d4a051] border-[2px] border-[#d4a051] rounded-full p-5"></i>
->>>>>>> Stashed changes
                   <h3 className="text-[21px]">Travel Arrangements</h3>
                   <p>
                     There are many variations of passages of Lorem Ipsum
                     available but the majority have suffered alteration.
                   </p>
-<<<<<<< Updated upstream
                     <button
               type="button"
               className="text-white bg-[#d4a051] group-hover:bg-white group-hover:text-black rounded-lg text-sm px-7 py-2.5 text-center  "
             >
              Read More
             </button>
-=======
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-[#903636] to-[#d4a051] hover:bg-gradient-to-l focus:ring-2 focus:outline-none focus:ring-[#903636] dark:focus:ring-purple-800 rounded-lg text-sm px-7 py-2.5 text-center"
-                  >
-                    Read More
-                  </button>
->>>>>>> Stashed changes
                 </div>
               </div>
               <div>
@@ -74,21 +59,12 @@ const ChooseUs = () => {
                     There are many variations of passages of Lorem Ipsum
                     available but the majority have suffered alteration.
                   </p>
-<<<<<<< Updated upstream
                     <button
               type="button"
               className="text-white bg-[#d4a051] group-hover:bg-white group-hover:text-black rounded-lg text-sm px-7 py-2.5 text-center  "
             >
              Read More
             </button>
-=======
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-[#903636] to-[#d4a051] hover:bg-gradient-to-l focus:ring-2 focus:outline-none focus:ring-[#903636] dark:focus:ring-purple-800 rounded-lg text-sm px-7 py-2.5 text-center"
-                  >
-                    Read More
-                  </button>
->>>>>>> Stashed changes
                 </div>
               </div>
               <div>
@@ -100,21 +76,12 @@ const ChooseUs = () => {
                     There are many variations of passages of Lorem Ipsum
                     available but the majority have suffered alteration.
                   </p>
-<<<<<<< Updated upstream
                     <button
               type="button"
               className="text-white bg-[#d4a051] group-hover:bg-white group-hover:text-black rounded-lg text-sm px-7 py-2.5 text-center  "
             >
              Read More
             </button>
-=======
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-[#903636] to-[#d4a051] hover:bg-gradient-to-l focus:ring-2 focus:outline-none focus:ring-[#903636] dark:focus:ring-purple-800 rounded-lg text-sm px-7 py-2.5 text-center"
-                  >
-                    Read More
-                  </button>
->>>>>>> Stashed changes
                 </div>
               </div>
               <div>
@@ -126,21 +93,12 @@ const ChooseUs = () => {
                     There are many variations of passages of Lorem Ipsum
                     available but the majority have suffered alteration.
                   </p>
-<<<<<<< Updated upstream
                     <button
               type="button"
               className="text-white bg-[#d4a051]  rounded-lg text-sm px-7 py-2.5 text-center  "
             >
              Read More
             </button>
-=======
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-[#903636] to-[#d4a051] hover:bg-gradient-to-l focus:ring-2 focus:outline-none focus:ring-[#903636] dark:focus:ring-purple-800 rounded-lg text-sm px-7 py-2.5 text-center"
-                  >
-                    Read More
-                  </button>
->>>>>>> Stashed changes
                 </div>
               </div>
             </Slider>
