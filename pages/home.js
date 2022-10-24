da66a4cf0b35159be9398dc8b01b6cd54c68340e
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-10-24 18:48:48 +0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -7,35 +7,11 @@ import Services from "../components/home/services/Services";
 import TopDestinations from "../components/home/top-destinations/TopDestinations";
 import Testimonial from "../components/home/testimonial/Testimonial";
 import Layout from "../components/Layout/Layout";
-import Masjid_al_Haram from "../assets/images/Masjid-ul-Haram-1.jpg";
-import Madina from "../assets/images/madina.jpg";
-import mosque from "../assets/images/mosque-gb86620f9b_1280.jpg";
 import IncrediblePlaces from "../components/home/incredible-places/IncrediblePlaces";
-
 import UseServices from "../hooks/UseServices";
-// const services = [
-//   {
-//     place: "Hajj",
-//     price: "7,00000",
-//     desc: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form.',
-//     img: Masjid_al_Haram,
-//   },
-//   {
-//     place: "Umrah",
-//     price: "2,00000",
-//     desc: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form.',
-//     img: Madina,
-//   },
-//   {
-//     place: "Turkey",
-//     price: "1,00000",
-//     desc: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form.',
-//     img: mosque,
-//   },
-// ];
 
 const Home = () => {
-    const services = UseServices();
+  const services = UseServices();
   return (
     <Layout>
       <Hero />
