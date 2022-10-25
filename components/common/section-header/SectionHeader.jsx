76f15e94b91857d70c78bb1c00e85511d436a1b1
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-10-25 17:34:44 +0500

    Update SectionHeader.jsx

--- components/common/section-header/SectionHeader.jsx
+++ components/common/section-header/SectionHeader.jsx
@@ -8,7 +8,7 @@ function SectionHeader({ headingPart1, headingPart2, subtitle, className }) {
         <span className="text-[#903636]"> {headingPart2}</span>
       </h3>
       {subtitle && (
-        <p className="text-[16px] md:text-[18px] leading-[20px] md:leading-[22px] text-[#333] dark:text-[#fff]">
+        <p className="text-[16px] md:text-[18px] leading-[20px] md:leading-[22px] dark:text-[#333]">
           {subtitle}
         </p>
       )}
