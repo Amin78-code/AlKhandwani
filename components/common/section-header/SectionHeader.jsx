38d46523cb56684464bae2d8d17fa06c426fc13a
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-10-25 17:37:38 +0500

    Update SectionHeader.jsx

--- components/common/section-header/SectionHeader.jsx
+++ components/common/section-header/SectionHeader.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function SectionHeader({ headingPart1, headingPart2, subtitle, className }) {
+function SectionHeader({
+  headingPart1,
+  headingPart2,
+  subtitle,
+  className,
+  whiteTheme,
+}) {
   return (
     <div className={`text-center text-[#d4a051] ${className}`}>
       <h3 className="text-[30px] md:text-[35px] leading-[36px] md:leading-[40px] font-bold uppercase">
@@ -8,7 +14,11 @@ function SectionHeader({ headingPart1, headingPart2, subtitle, className }) {
         <span className="text-[#903636]"> {headingPart2}</span>
       </h3>
       {subtitle && (
-        <p className="text-[16px] md:text-[18px] leading-[20px] md:leading-[22px] dark:text-[#333]">
+        <p
+          className={`text-[16px] md:text-[18px] leading-[20px] md:leading-[22px] ${
+            whiteTheme ? "dark:text-[#fff]" : "dark:text-[#333]"
+          }`}
+        >
           {subtitle}
         </p>
       )}
