d10519f150bf9dd3b41429d8b8e08cf1824e941a
Amin78-code <54574832+Amin78-code@users.noreply.github.com>
2022-10-25 18:24:01 +0500

    d

--- components/sevices-details/TourReviews.jsx
+++ components/sevices-details/TourReviews.jsx
@@ -4,14 +4,19 @@ import TourReviewsCard from "./TourReviewsCard";
 
 const TourReviews = ({ serviceDetails }) => {
   return (
-    <div className="w-full flex flex-col gap-y-[10px] pt-[10px] text-[15px] text-[#434e59]">
+    <div className="w-full flex flex-col gap-y-[10px] pt-[10px] text-[15px] text-[#434e59] dark:text-[#fff]">
       <h2 className="text-[21px] font-semibold mt-[20px] border-b border-[#e2cbcb] pb-[10px]">
         Reviews
       </h2>
-      {serviceDetails.reviews.length===0 && <p className="text-[20px] text-[#434e59]">There are no Reviews In this service!</p>}
-      {serviceDetails.reviews.length>0 && serviceDetails?.reviews.map((review) => {
-        return <TourReviewsCard review={review} key={review.id} />;
-      })}
+      {serviceDetails.reviews.length === 0 && (
+        <p className="text-[20px] text-[#434e59] dark:text-[#fff]">
+          There are no Reviews In this service!
+        </p>
+      )}
+      {serviceDetails.reviews.length > 0 &&
+        serviceDetails?.reviews.map((review) => {
+          return <TourReviewsCard review={review} key={review.id} />;
+        })}
     </div>
   );
 };
